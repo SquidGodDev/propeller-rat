a4ef9d0f5b7c93e74dd507a419e75af55bd7cf01
SquidGodDev <tako48188@gmail.com>
2024-06-01 19:02:04 -0700

    Added duplicate level names and extra start/end entity tests

--- source/scripts/tests.lua
+++ source/scripts/tests.lua
@@ -1,24 +1,31 @@
 function sanityChecks()
     local ldtk = LDtk
     local levelCount = ldtk.get_level_count()
+
+    local levelNames = {}
+
     for i=1, levelCount do
         local level = "Level_" .. i
         local levelName = ldtk.get_custom_data(level, "Name")
         assert(levelName, level .. " has no name")
 
-        local hasStart = false
-        local hasEnd = false
+        local duplicateName = levelNames[levelName]
+        assert(duplicateName == nil, level .. " has the same name as " .. (duplicateName or "") .. ": " .. levelName)
+        levelNames[levelName] = level
+
+        local startCount = 0
+        local endCount = 0
         for _, entity in ipairs(ldtk.get_entities(level)) do
             local entityName = entity.name
 
             if entityName == "Start" then
-                hasStart = true
+                startCount += 1
             elseif entityName == "End" then
-                hasEnd = true
+                endCount += 1
             end
         end
 
-        assert(hasStart, level .. " has no start")
-        assert(hasEnd, level .. " has no end")
+        assert(startCount == 1, level .. " has " .. startCount .. " start entities")
+        assert(endCount == 1, level .. " has " .. endCount .. " end entities")
     end
 end
