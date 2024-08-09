9a053529bcc78a224d1829e7d4f3410d6931a7fc
SquidGodDev <tako48188@gmail.com>
2024-08-08 20:20:02 -0700

    Clean up main.lua to remove unnecessary pd.update

--- source/main.lua
+++ source/main.lua
@@ -1,14 +1,4 @@
 -- Globals
 import "scripts/globals"
 
-local pd <const> = playdate
-local gfx <const> = pd.graphics
-
-function pd.update()
-    gfx.sprite.update()
-    pd.timer.updateTimers()
-
-    pd.drawFPS(0, 228)
-end
-
 SceneManager.startingScene(TitleScene)
